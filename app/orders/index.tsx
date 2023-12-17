dc91e062071557494ee8869145d5a581d7b9845f
tchisama <tchisama41@gmail.com>
2023-12-17 13:40:09 +0100

    make the folder route and make a sample header

---
+++ app/orders/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Index = (props: Props) => {
+  return (
+    <View>
+      <Text>orders page</Text>
+    </View>
+  )
+}
+
+export default Index
